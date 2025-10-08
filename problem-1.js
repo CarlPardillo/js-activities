2551ce40cc9d6e440a871e7352a8477bc6463e95
johndavebernardino-coder <johndave.bernardino@olivarezcollegetagaytay.edu.ph>
2025-10-08 11:30:56 +0800

    New activity

---
+++ problem-1.js
@@ -0,0 +1,10 @@
+//Create a program that will compute the sum and difference of the two numbers; 20 and 5.
+let no1 = 20
+let no2 = 5
+let sum = no1 + no2
+let diff = no1 - no2
+
+console.log("The first number is: " + no1);
+console.log("The second number is: " + no2);
+console.log("The sum of " + no1 + " and " + no2 + " is " + sum);
+console.log("The difference of " + no1 + " and " + no2 + " is " + diff);
---
+++ problem-2.js
@@ -0,0 +1,5 @@
+//Write a program that will convert the Fahrenheit value of 100 to Celsius.
+let fah = 100
+let fahconversion = fah - 32
+
+console.log("The celcius of 100 fahrenheit is: " + fahconversion * 0.5556 + " degree celcius")
